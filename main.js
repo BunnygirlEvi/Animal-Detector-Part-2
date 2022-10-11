d0665ebc01f9b8bac5b9543252293ed14afa384c
BunnygirlEvi <86636548+BunnygirlEvi@users.noreply.github.com>
2022-10-10 21:20:34 -0700

    Everything

---
+++ main.js
@@ -0,0 +1,13 @@
+function startClassification()
+{
+navigator.mediaDevices.getUserMedia({ audio: true});
+classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models.json', modelReady);
+}
+
+function modelReady() {
+    classifier.classify( gotResults);
+}
+
+function gotResults() {
+    console.log('Got Result!');
+}
